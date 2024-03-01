a3b1cfff5460478fe0eecb78d1750fd3dbe63a46
Hardik Raina <71309932+Raina-Hardik@users.noreply.github.com>
2024-03-01 15:27:08 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 import torch
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import StandardScaler
 
 base = SentenceTransformer("multi-qa-MiniLM-L6-cos-v1")
@@ -14,6 +15,9 @@ class LinearRegressionModel(nn.Module):
 
 def calc_sim(cell_1, cell_2):
     return util.dot_score(base.encode(cell_1), base.encode(cell_2))
+    
+def get_output(cell):
+    return model(cell)
 
 data = pd.read_csv('20_percent.csv')
 data['sim_score'] = data.apply(lambda row: calc_sim(row['desired_answer'], row['student_answer']), axis = 1)
@@ -22,3 +26,12 @@ model = LinearRegressionModel(input_size=1, output_size=1)
 model.load_state_dict(torch.load('lin_reg.pth'))
 model.eval()
 
+data['ouputs'] = data.apply(lambda row: get_output(row['sim_score']), axis = 1)
+
+from sklearn.metrics import mean_squared_error
+
+predicted_values = data['outputs']
+true_values = data['score_avg']
+
+rmse = np.sqrt(mean_squared_error(true_values, predicted_values))
+print(f'Root Mean Squared Error (RMSE): {rmse:.2f}')
