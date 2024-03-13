9651ac7af65bd06e43b0452cda3bc7f7d5f68a12
Hardik Raina <71309932+Raina-Hardik@users.noreply.github.com>
2024-03-13 20:50:10 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,37 +1,36 @@
-import torch
+import argparse
 import pandas as pd
 import numpy as np
-import torch.nn as nn
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 from sentence_transformers import SentenceTransformer, util
-base = SentenceTransformer("multi-qa-MiniLM-L6-cos-v1")
-
-class LinearRegressionModel(nn.Module):
-    def __init__(self, input_size, output_size):
-        super(LinearRegressionModel, self).__init__()
-        self.linear = nn.Linear(input_size, output_size)
-
-    def forward(self, x):
-        return self.linear(x)
 
 def calc_sim(cell_1, cell_2):
     return util.dot_score(base.encode(cell_1), base.encode(cell_2))
+
+def process_csv(csv_file):
+    base = SentenceTransformer("multi-qa-MiniLM-L6-cos-v1")
+    dataset = pd.read_csv(csv_file)
+
+    dataset['sim_score'] = dataset.apply(lambda row: calc_sim(row['desired_answer'], row['student_answer']), axis=1)
+    dataset['sim_score'] = dataset['sim_score'].apply(lambda x: x.item())
+    MAX_VAL = dataset['score_avg'].max()
+    dataset['predicted_score'] = dataset.apply(lambda row: row['sim_score'] * MAX_VAL, axis=1)
+    dataset.to_csv('processed.csv', index=False)
+
+    mae = np.mean(np.abs(dataset['predicted_score'] - dataset['score_avg']))
+    rmse = np.sqrt(np.mean((dataset['predicted_score'] - dataset['score_avg'])**2))
+
+    print("Mean Absolute Error (MAE):", mae)
+    print("Root Mean Square Error (RMSE):", rmse)
+
+def main():
+    parser = argparse.ArgumentParser(description="Process CSV file and calculate error metrics.")
+    parser.add_argument("csv_file", type=str, help="Path to the CSV file")
+    args = parser.parse_args()
     
-def get_output(cell):
-    return model(cell)
+    process_csv(args.csv_file)
 
-data = pd.read_csv('20_percent_split.csv')
-data['sim_score'] = data.apply(lambda row: calc_sim(row['desired_answer'], row['student_answer']), axis = 1)
-
-model = LinearRegressionModel(input_size=1, output_size=1)
-model.load_state_dict(torch.load('lin_reg.pth'))
-model.eval()
-
-data['outputs'] = data.apply(lambda row: get_output(row['sim_score']).detach().numpy(), axis = 1)
-
-from sklearn.metrics import mean_squared_error
-predicted_values = data['outputs']
-true_values = data['score_avg']
-rmse = np.sqrt(mean_squared_error(true_values, predicted_values))
-print(f'Root Mean Squared Error (RMSE): {rmse:.2f}')
+if __name__ == "__main__":
+    main()
